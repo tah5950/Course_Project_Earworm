2ae069387821eaee4dcb41cf9a5064ffab05a293
trevorhowat <tah5950@psu.edu>
2024-02-18 09:02:35 -0800

    Adjust Spacing on Webpage

--- EarwormWebApp/obj/Debug/net8.0/EarwormWebApp.AssemblyInfo.cs
+++ EarwormWebApp/obj/Debug/net8.0/EarwormWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EarwormWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7a62f3a719b7b8f151eee34e21cfc68160ead7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bcc15621222d90ab29006c2b2b99496ebf156d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EarwormWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EarwormWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
